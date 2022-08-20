50d80b165e2814f1f2366f123f60ae94636f83cd
PavelKrak <tropicpost@mail.ru>
2022-08-20 14:11:30 +0300

    second task

--- Seminar_1_HW/Program.cs
+++ Seminar_1_HW/Program.cs
@@ -1,24 +1,40 @@
 ﻿// Задача 2: Напишите программу, которая на вход
 // принимает два числа и выдаёт, какое число большее, а
 // какое меньшее.
-// Пример
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-if (a != b)
-{
-    if (a > b)
-    {
-        Console.WriteLine("число " + a + " больше числа " + b);
-    }
-    else Console.WriteLine("число " + b + " больше числа " + a);
+// if (a != b)
+// {
+//     if (a > b)
+//     {
+//         Console.WriteLine("число " + a + " больше числа " + b);
+//     }
+//     else Console.WriteLine("число " + b + " больше числа " + a);
 
-}
-else Console.WriteLine("числа не должны быть одинаковыми");
+// }
+// else Console.WriteLine("числа не должны быть одинаковыми");
+
+// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите три не одинаковых числа");
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine("Максимальное число из трёх введенных: " + max);
