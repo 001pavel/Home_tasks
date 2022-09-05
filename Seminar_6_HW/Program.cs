7fb510939ab57e6ca2365a1e7ad0ecb874d42e8c
PavelKrak <tropicpost@mail.ru>
2022-09-05 18:54:53 +0300

    Выполнение дополнительного задания на конвертирование из 10сс в 2. В двух вариантах

--- Seminar_6_HW/Program.cs
+++ Seminar_6_HW/Program.cs
@@ -19,13 +19,47 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Последовательно задайте значения b1, k1, b2 и k2: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-double k1 = Convert.ToInt32(Console.ReadLine());
-double b2 = Convert.ToInt32(Console.ReadLine());
-double k2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Последовательно задайте значения b1, k1, b2 и k2: ");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k2 * x + b2;
+// double x = (b2 - b1) / (k1 - k2);
+// double y = k2 * x + b2;
 
-Console.WriteLine($"Точка пересечения двух прямых заданных уравнениями y = {k1}x + {b1} и y = {k2}x + {b2} имеет координаты ({x}; {y}) ");
+// Console.WriteLine($"Точка пересечения двух прямых заданных уравнениями y = {k1}x + {b1} и y = {k2}x + {b2} имеет координаты ({x}; {y}) ");
+
+// Дополнительное задание: перевести число из 10 СС(системы счисления) в 2
+// Сложный вариант.
+
+string DesToBin(int numDes)
+{
+    string numBin = "";
+    while (numDes > 0)
+    {
+        if (numDes % 2 == 0) numBin += "0";
+        else numBin += "1";
+        numDes = numDes / 2;
+    }
+
+    string numBinRev = "";
+    for (int i = 0; i < numBin.Length; i++)
+    {
+        numBinRev += numBin[numBin.Length - 1 -i];
+    }
+    return numBinRev;
+}
+
+Console.Write("Введите число в десятичной системе исчисления: ");
+// int numDes = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат в двоичной системе: {DesToBin(Convert.ToInt32(Console.ReadLine()))}");
+
+// Простой вариант.
+string ConvertToBin(int num)
+{
+    return Convert.ToString(num, 2);
+}
+
+Console.Write("Введите число в десятичной системе исчисления: ");
+Console.WriteLine($"Результат в двоичной системе: {ConvertToBin(Convert.ToInt32(Console.ReadLine()))}");
